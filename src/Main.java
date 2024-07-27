f3a804bfeef0013d26d8142ad1d331d3ec935be7
Mike <mihlo4745@gmail.com>
2024-07-27 03:32:03 +0300

    More functional, robust, extensible, production-ready implementation

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,53 @@
 import java.util.*;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
+
     public static void main(String[] args) {
-        int[] arr = IntStream.rangeClosed(1, 100).toArray();
-
-        Map<Predicate<Integer>, String> preds = new LinkedHashMap<>();
-        preds.put(x -> x % 3 == 0, "Fizz");
-        preds.put(x -> x % 5 == 0, "Buzz");
-
-        System.out.println(fizzBuzz(arr, preds));
+        System.out.println(
+            mapMerge(
+                IntStream.rangeClosed(1, 100),
+                List.of(
+                    produceIf(then(remainder(3), eq(0)), "Fizz"),
+                    produceIf(then(remainder(5), eq(0)), "Buzz")
+                ),
+                String::concat, Integer::toString
+            ).collect(Collectors.joining(" "))
+        );
     }
 
-    private static String fizzBuzz(int[] arr, Map<Predicate<Integer>, String> preds) {
-        return Arrays.stream(arr)
-                .mapToObj(x -> preds.entrySet().stream()
-                        .filter(entry -> entry.getKey().test(x))
-                        .map(Map.Entry::getValue)
-                        .reduce(String::concat)
-                        .orElse(String.valueOf(x)))
-                .collect(Collectors.joining(" "));
+    private static <T> Stream<T> mapMerge(
+        IntStream input,
+        Collection<? extends IntFunction<? extends Stream<T>>> producers,
+        BinaryOperator<T> acc,
+        IntFunction<? extends T> fallback
+    ) {
+        return input
+            .mapToObj(x ->
+                producers
+                    .stream()
+                    .flatMap(p -> p.apply(x))
+                    .reduce(acc)
+                    .orElse(fallback.apply(x))
+            );
+    }
+
+    private static IntUnaryOperator remainder(int divisor) {
+        return x -> x % divisor;
+    }
+
+    private static IntPredicate eq(int toValue) {
+        return x -> x == toValue;
+    }
+    
+    private static IntPredicate then(IntUnaryOperator op, IntPredicate predicate) {
+        return x -> predicate.test(op.applyAsInt(x));
+    }
+
+    private static <T> IntFunction<Stream<T>> produceIf(IntPredicate predicate, T value) {
+        return x -> Stream.of(value).filter(s -> predicate.test(x));
     }
 }
